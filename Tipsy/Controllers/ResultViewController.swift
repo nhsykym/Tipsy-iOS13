de803eae7a9131dbe21543a1e8e70da049015886
Naohisa Yokoyama <nhsykym@gmail.com>
2020-05-20 18:06:44 +0900

    Tip calculator with multiple screen

---
+++ Tipsy/Controllers/ResultViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by 横山尚久 on 2020/05/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
